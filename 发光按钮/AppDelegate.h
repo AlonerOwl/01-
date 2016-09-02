49bdf7e00d9e04abb7dc24addc5562fbcdee0136
zhangfei <zhangfei@zhongchuangsanyou.com>
2016-09-02 11:07:01 +0800

    Initial Commit

---
+++ 发光按钮/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  发光按钮
+//
+//  Created by 张非 on 16/9/2.
+//  Copyright © 2016年 张非. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 发光按钮/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  发光按钮
+//
+//  Created by 张非 on 16/9/2.
+//  Copyright © 2016年 张非. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
